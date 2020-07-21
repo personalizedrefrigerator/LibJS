485cc02b8b7388c521061a8720f790954ee7b455
Henry Heino <personalizedrefrigerator@gmail.com>
2020-07-21 02:45:32 -0700

    Update python utils

--- Libs/Includes/printUtil.py
+++ Libs/Includes/printUtil.py
@@ -1,5 +1,7 @@
 #!python
 
+import sys
+
 FORMAT_COLORS = \
 {
     "GREEN": "\033[32m",
@@ -9,11 +11,12 @@ FORMAT_COLORS = \
 
 FORMAT_RESET = "\033[0m"
 FORMAT_OUTPUT = True
+COLOR_PRINT = sys.stdout.isatty()
 
 def cprint(text, color):
-    if color in FORMAT_COLORS and FORMAT_OUTPUT:
+    if color in FORMAT_COLORS and FORMAT_OUTPUT and COLOR_PRINT:
         print(FORMAT_COLORS[color] + str(text) + FORMAT_RESET, end='', flush=True)
-    elif type(color) == str:
+    elif type(color) == str and COLOR_PRINT:
         print(color + str(text) + FORMAT_RESET, end='', flush=True)
     else:
         print(text, end='', flush=True)
--- Libs/unify.py
+++ Libs/unify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os, os.path, sys
+from shlex import quote
 
 from Includes.printUtil import *
 from Includes.argsUtil import *
@@ -15,7 +16,7 @@ def merge(sourceDirectory):
 
     for fileName in files:
         if fileName.endswith(".js"):
-            contents += "\n// Inserted file %s encoding='%s'" % (fileName, ENCODING)
+            contents += "\n// Inserted file %s encoding='%s'" % (quote(fileName), ENCODING)
             contents += "\n"
 
             fileObj = open(os.path.join(sourceDirectory, fileName))
@@ -24,16 +25,63 @@ def merge(sourceDirectory):
 
     return contents
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: " + str(sys.argv[0]) + " [DIRNAME]", file=sys.stderr)
-        print("Sends output to stdout.", file=sys.stderr)
-        sys.exit(1)
 
-    dirName = sys.argv[1]
-    print("\"use strict\";") # Enforce strict mode.
-    print("(function()") # Wrap everything in a function...
-    print("{")
-    print(merge(dirName))
-    print("})();")
+def printHelp():
+    cprint("Help: \n", FORMAT_COLORS['YELLOW'])
+    cprint("  Summary: ", FORMAT_COLORS['YELLOW'])
+    print()
+    print("    Unify all JavaScript files in a directory. Does not support ES6 exports/modules.")
+    cprint("  Usage: ", FORMAT_COLORS['GREEN'])
+    print()
+    print("    python3 %s in/directory/here [--help|--exports]" % sys.argv[0])
+    print("  --help,-h         \t Print this help message.")
+    print("  --exports,-e      \t Read exports from stdin.")
+    print("  in/directory/here \t Unify all JavaScript files in this directory. Do not recurse.")
+    print("All output is sent to stdout. Exports should be a newline-separated list")
+    print("of the global-scope objects that should be exported from the given")
+    print("directory. For example,")
+    cprint(" SubWindowHelper", FORMAT_COLORS['GREEN'])
+    print()
+    cprint(" EditorHelper", FORMAT_COLORS['GREEN'])
+    print()
+    cprint(" JSHelper", FORMAT_COLORS['GREEN'])
+    print()
+    cprint(" HTMLHelper", FORMAT_COLORS['GREEN'])
+    print()
+    print("The above exports SubWindowHelper, EditorHelper, JSHelper, ")
+    print("and HTMLHelper to global scope. As such, including the generated")
+    print("JS file includes these objects in the project's global scope.")
+
+if __name__ == "__main__":
+
+    if __name__ == "__main__":
+        args = parseArgs(sys.argv,
+        {
+            'h': "help",
+            'e': "exports"
+        })
+        
+        if 'help' in args:
+            printHelp()
+        else:
+            # Print message to stderr because usually, output goes to stdout.
+            if not args['default']:
+                print("Usage: " + str(sys.argv[0]) + " [DIRNAME] ... other args ...", file=sys.stderr)
+                print("Sends output to stdout.", file=sys.stderr)
+                print()
+                sys.exit(1)
+
+            dirName = args['default'][0]
+            print("\"use strict\";") # Enforce strict mode.
+            print("(function()") # Wrap everything in a function...
+            print("{")
+            print(merge(dirName))
+
+            # If given exports, put them here.
+            if args['exports']:
+                for line in sys.stdin:
+                    exportName = line.rstrip()
+                    print("self.%s = %s;", exportName, exportName)
+
+            print("})();")
     
